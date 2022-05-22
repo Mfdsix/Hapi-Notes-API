946a5496efd329a8e6628ecf21b5f959a28852c1
Mahfudz Ainur Rif'an <Mfdsix.1nd0@gmail.com>
2022-05-22 15:44:41 +0700

    write routes

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,46 @@ const Handler = require('./handler')
 const routes = [
     {
         method: 'GET',
-        path: '/',
+        path: '/notes',
         handler: (req, h) => {
             return "hallo"
         }
-    }
+    },
+    {
+        method: 'POST',
+        path: '/notes',
+        handler: (req, h) => {
+            return "hallo"
+        }
+    },
+    {
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: (req, h) => {
+            return "hallo"
+        }
+    },
+    {
+        method: 'PUT',
+        path: '/notes/{id}',
+        handler: (req, h) => {
+            return "hallo"
+        }
+    },
+    {
+        method: 'DELETE',
+        path: '/notes/{id}',
+        handler: (req, h) => {
+            return "hallo"
+        }
+    },
+    {
+        method: '*',
+        path: '/{any*}',
+        handler: (req, h) => {
+            return "hallo"
+        }
+    },
 ]
 
 module.exports = routes
